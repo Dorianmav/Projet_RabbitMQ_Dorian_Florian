e825087b1d578c816ac6bb52ced2106ad888d06f
Dorianmav <dorianmav@outlook.com>
2024-06-23 16:26:58 +0200

    ajout du endpoint converssation pour avoir les messages

--- src/server.js
+++ src/server.js
@@ -21,7 +21,6 @@ app.use(cors(
 
 let channel;
 let connection;
-const messages = {}; // Store messages in memory
 
 // Initialisation de RabbitMQ
 async function initRabbitMQ() {
@@ -47,8 +46,6 @@ app.post('/register', async (req, res) => {
     });
     res.status(201).json(newUser);
 
-    // Démarrer la consommation des messages pour le nouvel utilisateur
-    consumeMessages(newUser);
   } catch (error) {
     res.status(400).json({ error: 'User registration failed' });
   }
@@ -66,14 +63,8 @@ app.post('/login', async (req, res) => {
     });
 
     if (user && await bcrypt.compare(password, user.password)) {
-      res.status(200).json({ message: 'Login successful', user });/* 
-      channel.consume(user.queue, (message) => {
-        readline.cursorTo(process.stdout, 0); // Move cursor to beginning of line
-        readline.clearLine(process.stdout, 1); // Clear line
-        const timestamp = new Date().toLocaleTimeString();
-        console.log(`Message received [${timestamp}]: ${message.content.toString()}`);
-        rl.prompt(true); // Re-print the prompt
-      }, { noAck: true }); */
+      res.status(200).json({ message: 'Login successful', user });
+
     } else {
       res.status(404).json({ error: 'User not found or incorrect password' });
     }
@@ -82,7 +73,6 @@ app.post('/login', async (req, res) => {
   }
 });
 
-// Route pour envoyer un message
 app.post('/send', async (req, res) => {
   const { userId, message, receiverId } = req.body;
 
@@ -99,63 +89,64 @@ app.post('/send', async (req, res) => {
       return res.status(404).json({ error: 'User or receiver not found' });
     }
 
-    const timestamp = new Date().toLocaleTimeString();
+    const timestamp = new Date();
     console.log(`[${user.name} Sent Message][${timestamp}]: ${message}`);
     channel.publish('direct_exchange', receiver.queue, Buffer.from(message));
+
+    // Enregistrer le message dans la base de données
+    await prisma.message.create({
+      data: {
+        content: message,
+        timestamp: timestamp,
+        senderId: userId,
+        receiverId: receiverId
+      }
+    });
+
     res.status(200).json({ message: 'Message sent' });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
 
-// Route pour récupérer les messages
-app.get('/receive/:userId', async (req, res) => {
-  const { userId } = req.params;
+app.get('/conversation/:userId1/:userId2', async (req, res) => {
+  const { userId1, userId2 } = req.params;
 
   try {
-    const user = await prisma.user.findUnique({
-      where: { id: parseInt(userId, 10) }
+    const user1 = await prisma.user.findUnique({
+      where: { id: parseInt(userId1, 10) }
     });
 
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+    const user2 = await prisma.user.findUnique({
+      where: { id: parseInt(userId2, 10) }
+    });
+
+    if (!user1 || !user2) {
+      return res.status(404).json({ error: 'One or both users not found' });
     }
 
-    res.status(200).json({ messages: messages[user.id] || [] });
+    const conversation = await prisma.message.findMany({
+      where: {
+        OR: [
+          { senderId: user1.id, receiverId: user2.id },
+          { senderId: user2.id, receiverId: user1.id }
+        ]
+      },
+      orderBy: {
+        timestamp: 'asc'
+      }
+    });
+
+    res.status(200).json({ conversation });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
 
-// Consommation des messages et stockage
-async function consumeMessages(user) {
-  await channel.assertQueue(user.queue, { durable: false });
-  await channel.bindQueue(user.queue, 'direct_exchange', user.queue);
-
-  channel.consume(user.queue, (message) => {
-    if (message !== null) {
-      const content = message.content.toString();
-      const timestamp = new Date().toLocaleTimeString();
-      console.log(`Message received by ${user.name} [${timestamp}]: ${content}`);
-
-      if (!messages[user.id]) {
-        messages[user.id] = [];
-      }
-      messages[user.id].push({ timestamp, content });
-
-      channel.ack(message);
-    }
-  });
-}
 
 // Démarrer le serveur et initialiser RabbitMQ
 app.listen(port, async () => {
   await initRabbitMQ();
-  // Consommer les messages pour tous les utilisateurs existants
-  const users = await prisma.user.findMany();
-  users.forEach(user => {
-    consumeMessages(user);
-  });
 
   console.log(`Server running on http://localhost:${port}`);
 });
